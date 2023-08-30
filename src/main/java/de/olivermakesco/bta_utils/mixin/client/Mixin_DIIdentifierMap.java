a50f291c3a2e2e311ce447959feda2f96fff4d86
Octal <oliver@olivermakesco.de>
2023-08-29 20:10:12 -0500

    Attempt to patch DIIdentifierMap#getButtonIdentifier

---
+++ src/main/java/de/olivermakesco/bta_utils/mixin/client/Mixin_DIIdentifierMap.java
@@ -0,0 +1,24 @@
+package de.olivermakesco.bta_utils.mixin.client;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.java.games.input.Component;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Environment(EnvType.CLIENT)
+@Mixin(targets = {"net.java.games.input.DIIdentifierMap"}, remap = false)
+public class Mixin_DIIdentifierMap {
+    @Inject(
+            method = "getButtonIdentifier",
+            at = @At("RETURN"),
+            cancellable = true
+    )
+    static void returnUnknownIfNull(int id, CallbackInfoReturnable<Component.Identifier.Button> cir) {
+        if (cir.getReturnValue() == null) {
+            cir.setReturnValue(Component.Identifier.Button.UNKNOWN);
+        }
+    }
+}
